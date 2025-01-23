1d9d2ea78fc59241a2d80d3f70d2d872a80cb8a9
Engrking10 <kingsley@conditionreports.com>
2025-01-23 02:34:17 -0500

    hide text

--- dealer2023-main/Demon0515-MongoDB-731/ClientApp/src/Pages/Game/Games.js
+++ dealer2023-main/Demon0515-MongoDB-731/ClientApp/src/Pages/Game/Games.js
@@ -30,8 +30,9 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   "&:nth-of-type(odd)": {
     backgroundColor: theme.palette.action.hover,
   },
-  // hide last border
-  "&:last-child td, &:last-child th": {
+    // hide last border
+  // hide
+  "&:last-child td, &:last-child th": {  
     border: 0,
   },
 }));
